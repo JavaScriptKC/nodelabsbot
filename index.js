93694ab04fc92c250e8621c29d989917918f3ca2
Andrew Dunkman <andrew@dunkman.org>
2012-10-27 10:45:50 -0500

    Added http listnener to stop heroku from crashing the process.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var irc = require("./lib/irc"),
-    twitter = require("./lib/twitter");
+    twitter = require("./lib/twitter"),
+    port = process.env.PORT || 0,
+    http = require("http"),
+    server = http.createServer();
 
 twitter.on("tweet", irc.say);
+server.listen(port);
